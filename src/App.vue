759880286f49979c941dfc88e64fd6e036e51728
koreyoshia <2659864189@qq.com>
2018-11-10 15:50:55 +0800

    Vue-cli

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <navigation :navTitle="propsTitle"></navigation>
+    <router-view @routerEmit='changeRouter'/>
+  </div>
+</template>
+<script>
+import navigation from '@/components/public/Navigation.vue'
+export default {
+  data(){
+    return{
+      propsTitle:"music"
+    }
+  },
+  components:{
+    navigation
+  },
+  methods:{
+    changeRouter(title){
+      this.propsTitle=title
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.container{
+  padding:1rem 0;
+}
+</style>
